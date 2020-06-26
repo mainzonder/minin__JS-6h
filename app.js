112020d4da2ed796047bd8718a0ba3eb87a1be11
mainzonder <48353743+mainzonder@users.noreply.github.com>
2020-06-26 14:19:30 +0300

    1hour

---
+++ app.js
@@ -0,0 +1,25 @@
+// 1 переменные
+// variable
+//camelCase
+
+const firstName = "tengu"; //string
+const isProgrammer = true; // boolean
+let age = 39; // number
+// name = "myname";
+age = 40;
+
+const $$ = "private";
+const $ = "some value";
+// const if-'mkef'  // err
+const withNum5 = "5";
+// const 5withNum = '5' // err
+
+//2 Мутирование
+
+// console.log(
+//   "Имя человека : " + firstName + " а возраст человека: " + age.toString()
+// );
+
+console.log(
+  "Имя человека : " + firstName + " а возраст человека: " + age.toString()
+);
